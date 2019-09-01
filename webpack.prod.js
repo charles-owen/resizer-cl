7fead8082255a972fe45a1728e136f621af8e961
cbowen <cbowen@cse.msu.edu>
2019-09-01 16:56:37 -0400

    Fixed dependency issues.

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,17 +1,17 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
 
 module.exports = merge(common, {
     mode: 'production',
     optimization: {
         minimizer: [
-            new UglifyJSPlugin({
+            new TerserPlugin({
                 cache: true,
                 parallel: true,
-                sourceMap: true // set to true if you want JS source maps
+                sourceMap: true  // Must be set to true if using source-maps in production
             })
         ]
     },
